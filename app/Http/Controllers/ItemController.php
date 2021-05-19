0e47fe03537dc7131120670f55ff66da519db2ab
whatwouldzoeldo <zulfikar91099@gmail.com>
2021-05-19 17:31:19 +0700

    API Commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class ItemController extends Controller
+{
+    //
+    function addItem (Request $req)
+    {
+        $item = new Item;
+        $item->judul=$req->input('judul');
+        $item->file_path=$req->file('foto')->store('items');
+        $item->kategori=$req->input('kategori');
+        $item->tipe=$req->input('tipe');
+        $item->keterangan=$req->input('keterangan');
+        $item->lokasi=$req->input('lokasi');
+        $item->kontak=$req->input('kontak');
+        $item->save();
+        return $item;
+    }
+
+    function listItem()
+    {
+        return Item::all();
+    }
+}
---
+++ database/migrations/2021_05_19_094433_create_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('file_path');
+            $table->string('kategori');
+            $table->string('tipe');
+            $table->string('keterangan');
+            $table->string('lokasi');
+            $table->string('kontak');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
